089f9422443701854256128782724fb8e9f0db23
smashingz <ad41nok@gmail.com>
2018-01-17 00:10:26 +0300

    It doesn't work. Need to count symbols in string.

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+SRC = lab1
 .PHONY: all clean
 
-all: main.cpp
-	g++ main.cpp `pkg-config --libs --cflags libpqxx`
+all: $(SRC).o
+	g++ -o $(SRC).out $(SRC).o `pkg-config --libs --cflags libpqxx`
+
+$(SRC).o: main.cpp
+	g++ -g -c -o $(SRC).o main.cpp
 
 clean:
 	rm -f *.o *.out
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <pqxx/connection>
 #include <pqxx/transaction>
 #include <iostream>
-#include <cstring>
+#include <iomanip>
+#include <string>
+#include <codecvt>
+
+void print_query_res(pqxx::result r);
 
 int main() {
 	const std::string DB_NAME = "hostels";
@@ -25,19 +29,50 @@ int main() {
                    std::cout << "Empty result set." << std::endl;
                    return 0;
 		}
-		for(int i = 0; i < res.columns(); i++) {
-			std::cout << res.column_name(i) << "\t";
-		}
-		std::cout << std::endl;
-		//TODO Сделать нормальный вывод таблицы
-		for (auto i: res) {
-			for (auto j: i) {
-				std::cout << j << "\t";
-			}
-				std::cout << std::endl;
-		}
+	print_query_res(res);
 	} catch(pqxx::pqxx_exception &e) {
 		std::cerr << e.base().what() << std::endl;
 	}
 	return 0;
+}
+
+void print_query_res(pqxx::result r) {
+		int n = 0, sum = 0;
+		std::vector<int> widths(r.columns());
+		for(pqxx::tuple::size_type i = 0; i < r.columns(); i++) {
+			widths[i] = std::string(r.column_name(i)).length();
+		}
+		for (auto i: r) {
+			for (auto j: i) {
+			//	std::cout << "[" << j << "] ";
+				if(pqxx::to_string(j).length() > widths[n]) {
+					//std::cout << pqxx::to_string(j) << "[" << pqxx::to_string(j).length() << "]" << std::endl;
+					//widths[n] = std::wstring(pqxx::to_string(j)).length();
+					std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+					std::string narrow = converter.to_bytes(pqxx::to_string(j));
+					std::wstring wide = converter.from_bytes(narrow); // TODO Сделать нормальный подсчёт символов в строке
+					width[n] = wide.length();
+				}
+				n++;
+			}
+			n = 0;
+			//std::cout << std::endl;
+		}
+		for(int i = 0; i < r.columns(); i++) {
+			std::cout << "|" << std::setw(widths[i]) << std::left << r.column_name(i) << " ";
+		}
+		std::cout << std::endl;
+		for(auto i: widths) {
+			sum += i;
+		}
+		std::cout << std::setfill('-') << std::setw(sum) << "-" << std::endl;
+		std::cout << std::setfill(' ');
+		n = 0;
+		for (auto i: r) {
+			for (auto j: i) {
+				std::cout << "|" << std::setw(widths[n++]) << std::left << j << " ";
+			}
+				std::cout << std::endl;
+				n = 0;
+		}
 }
